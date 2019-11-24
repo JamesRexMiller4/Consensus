a22575927616515abedf9abbe7cb3d0d774b572c
James Miller <james.rex.miller.4@gmail.com>
2019-11-24 11:39:09 -0700

    Add gitignore file

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+})
+
+// this code determines our port from Heroku or 5000 by default
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
