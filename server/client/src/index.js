e7648e7416d604c96c7d51a8aae754fee704dbb9
James Miller <james.rex.miller.4@gmail.com>
2019-11-30 11:07:31 -0800

    Add materialize-css, add header styles

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,14 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+ 
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, 
+    document.getElementById('root'));
