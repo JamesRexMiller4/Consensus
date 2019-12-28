d822c32906b92c4233e1d847dad8819cc2f70674
James Miller <james.rex.miller.4@gmail.com>
2019-12-27 18:56:49 -0700

    Add redirect for once logged in and logout

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
