a3573e40cb05a09717529d7cacf9dea84b4a84eb
James Miller <james.rex.miller.4@gmail.com>
2019-12-27 19:28:12 -0700

    Add conditional redirect for Header logo Link

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -17,7 +17,9 @@ class Header extends Component {
     return (
     <nav>
       <div className="nav-wrapper">
-        <a href="#" className="left L1brand-logo">Consensus</a>
+        <Link 
+        to={this.props.auth ? '/surveys' : '/'} 
+        className="left brand-logo">Consensus</Link>
         <ul id="nav-mobile" className="right">
           {this.renderContent()}
         </ul>
