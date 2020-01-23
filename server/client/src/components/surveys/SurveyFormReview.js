a74a9d61aa9905bfc8fb84a5f13b7900f8cfc206
James Miller <james.rex.miller.4@gmail.com>
2020-01-23 08:53:41 -0700

    Add redirect functionality and complete submitSurvey functionality

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,10 @@ import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import formFields from './formFields';
-import * as actions from '../../actions/index';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ name, label}) => {
     return (
       <div key={name}>
@@ -22,7 +23,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
       <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
         Back
       </button>
-      <button onClick={() => submitSurvey(formValues)} className="green btn-flat white-text right">
+      <button onClick={() => submitSurvey(formValues, history)} className="green btn-flat white-text right">
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -34,4 +35,4 @@ const mapStateToProps = state => ({
   formValues: state.form.surveyForm.values
 })
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
