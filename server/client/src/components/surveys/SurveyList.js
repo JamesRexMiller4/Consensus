eac304d8ff555bc68c12801459579bbd301d50df
James Miller <james.rex.miller.4@gmail.com>
2020-01-24 13:07:04 -0700

    Add SurveyList component and render to the Dashboard + styles

---
+++ server/client/src/components/surveys/SurveyList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions/index';
+
+class SurveyList extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount(props) {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card darken-1" key={survey.id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+              {survey.body}
+            </p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+            <div className="card-action">
+              <a>Yes: {survey.yes}</a>
+              <a>No: {survey.no}</a>
+            </div>
+          </div>
+        </div>
+      )
+    });
+  }
+
+  render() { 
+    return ( 
+      <div>
+        {this.renderSurveys()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ surveys }) => ({
+  surveys
+})
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
